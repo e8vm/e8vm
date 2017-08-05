c47b2c49e8c574ade291c9eef261189e994510f4
He Liu <liulonnie@gmail.com>
2017-08-04 19:13:45 -0700

    .

--- dagvis/repo.go
+++ dagvis/repo.go
@@ -3,14 +3,14 @@ package dagvis
 // Repo is the overview dependency structure of a repository.
 type Repo struct {
 	Name     string
-	PkgDep   *M
-	FileDeps map[string]*M
+	RepoTopo *M
+	PkgTopos map[string]*M
 }
 
 // NewRepo creates an empty overview for a repo.
 func NewRepo(name string) *Repo {
 	return &Repo{
 		Name:     name,
-		FileDeps: make(map[string]*M),
+		PkgTopos: make(map[string]*M),
 	}
 }
